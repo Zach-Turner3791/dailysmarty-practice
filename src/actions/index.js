1eaf0149b8032ba40103bd8265f2c88e50b10de4
Zach-Turner3791 <zachary.turner9012@gmail.com>
2023-02-21 11:18:29 -0700

    commit

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+
+
+export function fetchRecentPosts() {
+  return function(dispatch) {
+    axios
+      .get('https//api.dailysmarty.com/posts')
+      .then(response => {
+        console.log(response.data, 'response')
+      })
+  }
+}
